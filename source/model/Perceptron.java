cf143a81e49e1c01097a6264e626026f32086720
Hi_Crame <343548912@qq.com>
2026-01-21 22:56:58 +0800

    feat(core): 添加基本的数据结构和矩阵操作类
    添加了一个新的CSV文件`and.csv`，包含逻辑与运算的真值表。
    
    在README.md中添加了项目的详细描述，包括主要组成部分和数据结构单元的说明。
    
    新增了`Perceptron.java`文件，定义了一个简单的感知器模型类，包含主函数以初始化模型。
    
    新增了`Matrix.java`文件，定义了一个矩阵类，提供了默认构造函数、指定行数和列数的构造函数、使用数组初始化的构造函数、拷贝构造函数以及各种获取和设置元素的方法。
    
    新增了`Vector.java`文件，定义了一个向量类，提供了默认构造函数、指定长度的构造函数、使用数组初始化的构造函数、拷贝构造函数以及各种获取和设置元素的方法。
    
    新增了`AdvanceMatrixMath.java`文件，定义了一个高级矩阵数学计算类，实现了矩阵乘法、行列式计算、迹计算、秩计算、余子式矩阵获取和逆矩阵计算等复杂操作。
    
    新增了`MatrixMath.java`文件，定义了一个矩阵数学计算类，提供了矩阵的常数填充、按行和按列序列化填充、随机数填充、矩阵加法、减法、数乘、标量加法和减法、以及矩阵转置等操作。
    
    新增了`VectorMath.java`文件，定义了一个向量数学计算类，提供了向量的range序列化生成、常数填充、随机数填充、加法、减法、数乘、标量加法和减法、点乘、模计算、单位化和投影等操作。

---
+++ source/model/Perceptron.java
@@ -0,0 +1,7 @@
+package source.model;
+
+public class Perceptron {
+    public static void main(String[] args) {
+        System.out.println("Perceptron model initialized.");
+    }
+}
---
+++ source/util/MatrixMath.java
@@ -0,0 +1,209 @@
+package source.util;
+
+import source.unit.Matrix;
+import source.unit.Vector;
+
+/**
+ *  @HiCharm
+ *  @Date: 2026-01-21 21:38
+ *  @Description: 该矩阵数学计算类，用于构建矩阵，以及矩阵计算
+ */
+public class MatrixMath {
+    /**
+     * 常数矩阵填充
+     */
+    public static void fillMatrix(double value, Matrix matrix){
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            for (int j = 0; j < matrix.getWidth(); j++) {
+                matrix.rows.get(i).set(j, value);
+            }
+        }
+    }
+
+    /**
+     * 矩阵按行序列化填充
+     */
+    public static void rowMatrix(int start, int end, int step, Matrix matrix) {
+        if(matrix.getWidth() != ((end - start) / step + 1)) {
+            throw new IllegalArgumentException("Matrix width does not match the specified range and step.");
+        }
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.rangeVector(start, end, step, matrix.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵按列序列化填充
+     */
+    public static void columnMatrix(int start, int end, int step, Matrix matrix) {
+        if(matrix.getHeight() != ((end - start) / step + 1)) {
+            throw new IllegalArgumentException("Matrix height does not match the specified range and step.");
+        }
+        for (int i = 0; i < matrix.getWidth(); i++) {
+            Vector column = new Vector(matrix.getHeight());
+            for (int j = 0; j < matrix.getHeight(); j++) {
+                column.set(j, matrix.rows.get(j).get(i));
+            }
+            matrix.setColumn(i, column);
+        }
+    }
+
+    /**
+     * 矩阵的随机数填充
+     */
+    public static void randomMatrix(double min, double max, Matrix matrix){
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.randomVector(min, max, matrix.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵加法，无返回值，不覆盖
+     */
+    public static void add(Matrix m1, Matrix m2, Matrix result) {
+        if (m1.getHeight() != m2.getHeight() || m1.getWidth() != m2.getWidth()) {
+            throw new IllegalArgumentException("Matrices must be of the same dimensions for addition.");
+        }
+        if (result.getHeight() != m1.getHeight() || result.getWidth() != m1.getWidth()) {
+            throw new IllegalArgumentException("Result matrix must be of the same dimensions as the input matrices for addition.");
+        }
+        for (int i = 0; i < m1.getHeight(); i++) {
+            VectorMath.add(m1.rows.get(i), m2.rows.get(i), result.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵加法，无返回值，覆盖
+     */
+    public static void add(Matrix m1, Matrix m2, boolean left){
+        if (left) {
+            for (int i = 0; i < m1.getHeight(); i++) {
+                VectorMath.add(m1.rows.get(i), m2.rows.get(i), true);
+            }
+        } else {
+            for (int i = 0; i < m1.getHeight(); i++) {
+                VectorMath.add(m1.rows.get(i), m2.rows.get(i), false);
+            }
+        }
+    }
+
+    /**
+     * 矩阵减法，无返回值，不覆盖
+     */
+    public static void subtract(Matrix m1, Matrix m2, Matrix result) {
+        if (m1.getHeight() != m2.getHeight() || m1.getWidth() != m2.getWidth()) {
+            throw new IllegalArgumentException("Matrices must be of the same dimensions for subtraction.");
+        }
+        if (result.getHeight() != m1.getHeight() || result.getWidth() != m1.getWidth()) {
+            throw new IllegalArgumentException("Result matrix must be of the same dimensions as the input matrices for subtraction.");
+        }
+        for (int i = 0; i < m1.getHeight(); i++) {
+            VectorMath.subtract(m1.rows.get(i), m2.rows.get(i), result.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵减法，无返回值，覆盖
+     */
+    public static void subtract(Matrix m1, Matrix m2, boolean left){
+        if (left) {
+            for (int i = 0; i < m1.getHeight(); i++) {
+                VectorMath.subtract(m1.rows.get(i), m2.rows.get(i), true);
+            }
+        } else {
+            for (int i = 0; i < m1.getHeight(); i++) {
+                VectorMath.subtract(m1.rows.get(i), m2.rows.get(i), false);
+            }
+        }
+    }
+
+    /**
+     * 矩阵数乘，无返回值，不覆盖
+     */
+    public static void multiply(double scalar, Matrix matrix, Matrix result) {
+        if (result.getHeight() != matrix.getHeight() || result.getWidth() != matrix.getWidth()) {
+            throw new IllegalArgumentException("Result matrix must be of the same dimensions as the input matrix for multiplication.");
+        }
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.multiply(scalar, matrix.rows.get(i), result.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵数乘，无返回值，覆盖
+     */
+    public static void multiply(double scalar, Matrix matrix){
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.multiply(scalar, matrix.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵标量加，不覆盖
+     */
+    public static void scalarAdd(double scalar, Matrix matrix, Matrix result) {
+        if (result.getHeight() != matrix.getHeight() || result.getWidth() != matrix.getWidth()) {
+            throw new IllegalArgumentException("Result matrix must be of the same dimensions as the input matrix for scalar addition.");
+        }
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.add(scalar, matrix.rows.get(i), result.rows.get(i));
+        }
+    }
+    /**
+     * 矩阵标量加，覆盖
+     */
+    public static void scalarAdd(double scalar, Matrix matrix){
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.add(scalar, matrix.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵标量减，不覆盖
+     */
+    public static void scalarSubtract(double scalar, Matrix matrix, Matrix result) {
+        if (result.getHeight() != matrix.getHeight() || result.getWidth() != matrix.getWidth()) {
+            throw new IllegalArgumentException("Result matrix must be of the same dimensions as the input matrix for scalar subtraction.");
+        }
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.subtract(scalar, matrix.rows.get(i), result.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵标量减，覆盖
+     */
+    public static void scalarSubtract(double scalar, Matrix matrix){
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            VectorMath.subtract(scalar, matrix.rows.get(i));
+        }
+    }
+
+    /**
+     * 矩阵转置，不覆盖
+     */
+    public static void transpose(Matrix matrix, Matrix result) {
+        if (result.getHeight() != matrix.getWidth() || result.getWidth() != matrix.getHeight()) {
+            throw new IllegalArgumentException("Result matrix must be of the dimensions of the transposed matrix.");
+        }
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            for (int j = 0; j < matrix.getWidth(); j++) {
+                result.set(j, i, matrix.get(i, j));
+            }
+        }
+    }
+
+    /**
+     * 矩阵转置，覆盖
+     */
+    public static void transpose(Matrix matrix){
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            for (int j = i + 1; j < matrix.getHeight(); j++) {
+                double temp = matrix.get(i, j);
+                matrix.set(i, j, matrix.get(j, i));
+                matrix.set(j, i, temp);
+            }
+        }
+    }
+
+}
